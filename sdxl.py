84c8ce804221c507c0b3f997065a4dd2f3866964
Will Berman <WLBberman@gmail.com>
2023-09-15 00:13:34 +0000

    note

--- sdxl.py
+++ sdxl.py
@@ -539,6 +539,9 @@ def sdxl_log_validation(step):
                         validation_image, return_type="pil"
                     )
                 elif training_config.controlnet_type == "inpainting":
+                    # TODO - because we can't get a PIL back here to pass to both the 
+                    # wandb lob and the pipeline, this is messy+redundant. Is there
+                    # a better way to do this?
                     from masking import make_masked_image
 
                     validation_image = make_masked_image(
