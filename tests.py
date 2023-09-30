d7a442e6cfdf0847b9a1c2737f1fffcaac752d9f
William Berman <WLBberman@gmail.com>
2023-09-30 22:27:25 +0200

    init tests

---
+++ tests.py
@@ -0,0 +1,44 @@
+class TestTrainingLoop:
+    def test_save_checkpoint(self):
+        ...
+
+
+class TestSDXLTraining:
+    ...
+
+
+class TestSDXLDataset:
+    ...
+
+
+class TestSDXLInference:
+    def test_sdxl_diffusion_loop(self):
+        ...
+
+    def test_gen_sdxl_simplified_interface(self):
+        ...
+
+
+class TestSDXLModels:
+    def test_sdxl_vae(self):
+        ...
+
+    def test_sdxl_unet(self):
+        ...
+
+    def test_sdxl_controlnet(self):
+        ...
+
+    def test_sdxl_pre_encoded_controlnet_cond(self):
+        ...
+
+    def test_sdxl_controlnet_full(self):
+        ...
+
+    def test_sdxl_adapter(self):
+        ...
+
+
+class TestDiffusion:
+    def test_ode_solver_diffusion_loop(self):
+        ...
