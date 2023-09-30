3826ccc0e09b8b24b4bc8eeb3dc0ce56cad6ccc1
William Berman <WLBberman@gmail.com>
2023-09-30 20:46:17 +0200

    fix

--- misc/sdxl_vae_encode_decode.py
+++ misc/sdxl_vae_encode_decode.py
@@ -6,9 +6,8 @@ def main():
     vae = SDXLVae.load_fp16_fix(device='cuda')
     
     image = Image.open("./validation_data/two_birds_on_branch.png")
+    image = image.convert('RGB')
     image = image.resize((1024, 1024))
-    image = TF.to_tensor(image)
-    image = TF.normalize(image, [0.5], [0.5])
     image = vae.input_pil_to_tensor(image)
     image = image.to(device=vae.device, dtype=vae.dtype)
 
